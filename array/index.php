dcb0bd4f2d69eaf8f64d601e153f850e34d1fd38
Taha Amin Ghafuri <82469904+tahaghafuri@users.noreply.github.com>
2022-06-20 22:21:32 +0430

    Create index.php

---
+++ array/index.php
@@ -0,0 +1,28 @@
+<?php
+$array=['0987654321'=>['firstname'=>'Ali','lastname'=>'Mohammadi','age'=>31,'gender'=>'Man'],'0987654322'=>['firstname'=>'Rea','lastname'=>'Mohammadi','age'=>32,'gender'=>'Man'],'0987654323'=>['firstname'=>'Behruz','lastname'=>'Mohammadi','age'=>33,'gender'=>'Woman']];
+?>
+<style>
+table,td,th,tr{
+    border-collapse:collapse;
+    border:1px black solid;
+    padding: 5px;
+}
+</style>
+<table>
+<tr>
+    <td>نام</td>
+    <td>نام خانوادگی</td>
+    <td>سن</td>
+    <td>جنسیت</td>
+    <td>کدملی</td>
+</tr>
+<?php foreach($array as $a => $info){ ?>
+<tr>
+    <td><?php echo $info['firstname']; ?></td>
+    <td><?php echo $info['lastname']; ?></td>
+    <td><?php echo $info['age']; ?></td>
+    <td><?php echo $info['gender']; ?></td>
+    <td><?php echo $a; ?></td>
+</tr>
+<?php } ?>
+</table>
